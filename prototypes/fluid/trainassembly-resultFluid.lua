256e3e9dedd1aa5940f4078740e3c1d0222e3ef1
LovelySanta <laurens.scheldeman@gmail.com>
2018-07-02 23:17:56 +0200

    Fixed small issue with icon

--- prototypes/fluid/trainassembly-resultFluid.lua
+++ prototypes/fluid/trainassembly-resultFluid.lua
@@ -23,16 +23,16 @@ for _, itemName in pairs(trainassemblyItemTable) do
 
   local itemFluid = util.table.deepcopy(data.raw["fluid"]["crude-oil"])
 
-  itemFluid.name            = itemName .. "-fluid"
-  itemFluid.icon            = util.table.deepcopy(data.raw[item-with-entity-data][itemName].icon)
-  itemFluid.icon_size       = util.table.deepcopy(data.raw[item-with-entity-data][itemName].icon_size)
-  itemFluid.order           = util.table.deepcopy(data.raw[item-with-entity-data][itemName].order)
-  itemFluid.auto_barrel     = false
-  itemFluid.subgroup        = "transport-trains"
+  itemFluid.name        = itemName .. "-fluid"
+  itemFluid.icon        = data.raw["item-with-entity-data"][itemName].icon
+  itemFluid.icon_size   = data.raw["item-with-entity-data"][itemName].icon_size
+  itemFluid.icons       = util.table.deepcopy(data.raw["item-with-entity-data"][itemName].icons)
+  itemFluid.order       = data.raw["item-with-entity-data"][itemName].order
+  itemFluid.auto_barrel = false
+  itemFluid.subgroup    = "transport-trains"
 
   data:extend({
     itemFluid,
-
   })
 
 end
