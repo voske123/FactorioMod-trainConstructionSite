db476a7a464f9218fe86da411a7c7bea0ddc289b
voske123 <reynaert_123@skynet.be>
2018-07-04 17:28:31 +0200

    adjusted trainassembly-resultfluid

--- prototypes/fluid/trainassembly-resultFluid.lua
+++ prototypes/fluid/trainassembly-resultFluid.lua
@@ -1,7 +1,5 @@
 require "util"
 
-local trainassemblyItemTable = {}
-
 for _, trainType in pairs({
     "locomotive",
     "cargo-wagon",
@@ -12,25 +10,22 @@ for _, trainType in pairs({
   for _,trainEntity in pairs(data.raw[trainType]) do
 
     if trainEntity.minable and trainEntity.minable.result then
-        table.insert(trainassemblyItemTable, trainEntity.minable.result)
+
+        local itemName = trainEntity.minable.result
+        local itemFluid = util.table.deepcopy(data.raw["fluid"]["crude-oil"])
+
+        itemFluid.name        = itemName .. "-fluid"
+        itemFluid.icon        = data.raw["item-with-entity-data"][itemName].icon
+        itemFluid.icon_size   = data.raw["item-with-entity-data"][itemName].icon_size
+        itemFluid.icons       = util.table.deepcopy(data.raw["item-with-entity-data"][itemName].icons)
+        itemFluid.order       = data.raw["item-with-entity-data"][itemName].order
+        itemFluid.auto_barrel = false
+        itemFluid.subgroup    = "transport-trains"
+
+        data:extend({
+          itemFluid,
+        })
+
     end
   end
-end
-
-for _, itemName in pairs(trainassemblyItemTable) do
-
-  local itemFluid = util.table.deepcopy(data.raw["fluid"]["crude-oil"])
-
-  itemFluid.name        = itemName .. "-fluid"
-  itemFluid.icon        = data.raw["item-with-entity-data"][itemName].icon
-  itemFluid.icon_size   = data.raw["item-with-entity-data"][itemName].icon_size
-  itemFluid.icons       = util.table.deepcopy(data.raw["item-with-entity-data"][itemName].icons)
-  itemFluid.order       = data.raw["item-with-entity-data"][itemName].order
-  itemFluid.auto_barrel = false
-  itemFluid.subgroup    = "transport-trains"
-
-  data:extend({
-    itemFluid,
-  })
-
 end
