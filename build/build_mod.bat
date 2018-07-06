0a55e1b9b19112451e35095f59c7e543e9b41dc0
LovelySanta <laurens.scheldeman@gmail.com>
2018-07-06 22:17:39 +0200

    Extracted modinfo

--- build/build_mod.bat
+++ build/build_mod.bat
@@ -2,5 +2,5 @@
 @echo off
 if not "%1" == "max" start /MAX cmd /c %0 max & exit/b
 
-cmd /c gulp --color --gulpfile "./gulp_build_mod.js" main
+cmd /c gulp --silent --color --gulpfile "./gulp_build_mod.js" main
 pause
--- build/gulp_build_mod.js
+++ build/gulp_build_mod.js
@@ -1,22 +1,68 @@
-var gulp     = require('gulp')
-var gulpUtil = require('gulp-util')
+// example https://pastebin.com/embed_iframe/faVj8bXA
+
+var gulp     = require("gulp");
+var fs = require("fs");
+
+let modName, modVersion;
+
 
 gulp.task('log1', () => {
+  console.log("== My First Log Task ==");
+
   return new Promise(function(resolve, reject) {
-    gulpUtil.log("== My First Log Task ==");
     resolve();
   });
 
+});
+
+
+
+gulp.task("build_modinfo", () => {
+  console.log("\n== Extracting mod info ==");
+
+  let modInfo
+  try {
+    fs.accessSync("../info.json", fs.constants.R_OK | fs.constants.W_OK);
+    modInfo = JSON.parse(fs.readFileSync("../info.json").toString());
+  } catch (e) {
+    throw new Error("The file [info.json] was not readable/writable or may not exist at all please run a map for more than 2 seconds after tab or create the file manually");
+  }
+
+  modName    = modInfo.name;
+  modVersion = modInfo.version
+    .split(".")
+    .map(function(v) {return parseInt(v);})
+    .join(".");
+
+  return new Promise(function(resolve, reject) {
+    console.log("   Name    : " + modName    );
+    console.log("   Version : " + modVersion );
+
+    resolve();
+  });
 });
 
 gulp.task('log2', () => {
+  console.log("== My Second Log Task ==");
+
   return new Promise(function(resolve, reject) {
-    gulpUtil.log("== My Second Log Task ==");
     resolve();
   });
 });
 
+
+
+
+// Task building the zip file
+gulp.task("build_process", gulp.series(
+  "build_modinfo",
+  "log1"
+));
+
+
+
+// Main program
 gulp.task("main", gulp.series(
-  'log1',
-  'log2'
+  "build_process",
+  "log2"
 ));
