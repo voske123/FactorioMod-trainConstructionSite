bd9e211338dfcf8ce4fa6a5c54f1ee46d67b4386
voske123 <reynaert_123@skynet.be>
2018-07-07 23:48:33 +0200

    Delete loc and cargo wagon

--- prototypes/technology/trainassembly-technology.lua
+++ prototypes/technology/trainassembly-technology.lua
@@ -1,9 +1,29 @@
 
-  if not data.raw["technology"]["railway"].effects then
-    data.raw["technology"]["railway"].effects = {}
+if data.raw["technology"]["railway"].effects then
+  for effectIndex, effect in pairs(data.raw["technology"]["railway"].effects) do
+    if effect.type == "unlock-recipe"
+    and (effect.recipe == "locomotive" or effect.recipe == "cargo-wagon") then
+      data.raw["technology"]["railway"].effects[effectIndex] = nil -- this removes that single unlock
+    end
   end
 
-  table.insert(data.raw["technology"]["railway"].effects, {
+else
+  data.raw["technology"]["railway"].effects = {}
+end
+
+for _, fuelIngredient in pairs ({
+  "raw-wood",
+  "coal",
+}) do
+  table.insert(data.raw["technology"]["railway"].effects,
+  {
     type = "unlock-recipe",
-    recipe = "trainassembly",
+    recipe = "trainassembly-trainfuel-" .. fuelIngredient,
   })
+end
+
+table.insert(data.raw["technology"]["railway"].effects,
+{
+  type = "unlock-recipe",
+  recipe = "trainassembly",
+})
