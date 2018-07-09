65aa81f8599fa5dc4e4fd67d381b7690f5c7dd16
voske123 <reynaert_123@skynet.be>
2018-07-09 09:40:47 +0200

    changed subgroups of some items

--- data.lua
+++ data.lua
@@ -6,6 +6,7 @@ require "prototypes.categories.groups"
 require "prototypes.item.trainassembly-placeable"
 require "prototypes.item.trainassembly-fuel"
 require "prototypes.item.trainassembly-resultItem"
+require "prototypes.item.trainassembly-subgroups"
 
 require "prototypes.fluid.trainassembly-resultFluid"
 
---
+++ prototypes/item/trainassembly-subgroups.lua
@@ -0,0 +1,5 @@
+
+data.raw["item"]["rail-signal"].subgroup = data.raw["item-subgroup"]["transport-railway"].name
+data.raw["item"]["rail-chain-signal"].subgroup = data.raw["item-subgroup"]["transport-railway"].name
+data.raw["rail-planner"]["rail"].subgroup = data.raw["item-subgroup"]["transport-railway"].name
+data.raw["item"]["train-stop"].subgroup = data.raw["item-subgroup"]["transport-railway"].name
