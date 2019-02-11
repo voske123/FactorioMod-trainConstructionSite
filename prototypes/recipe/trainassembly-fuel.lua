cebb8bbba4efa09649041a8e5f0bad308f937654
voske123 <reynaert_123@skynet.be>
2019-02-11 22:58:40 +0100

    #26 Added trainfuel icon layer

--- prototypes/recipe/trainassembly-fuel.lua
+++ prototypes/recipe/trainassembly-fuel.lua
@@ -1,5 +1,46 @@
 require 'util'
 
+local function createRecipeIcons(itemPrototypeName)
+  local function getItemIcons(itemPrototypeName, layerScale, layerShift)
+    local itemIcon = data.raw["item"][itemPrototypeName].icon
+
+    if itemIcon then
+      return { -- icons table
+        { -- single layer
+          ["icon"] = itemIcon,
+          ["icon_size"] = data.raw["item"][itemPrototypeName].icon_size,
+          ["scale"] = layerScale,
+          ["shift"] = layerShift,
+        }
+      }
+    else
+
+      local itemIconSize = data.raw["item"][itemPrototypeName].icon_size
+
+      local itemIcons = {}
+      for iconLayerIndex, iconLayer in pairs(data.raw["item"][itemPrototypeName].icons) do
+        itemIcons[iconLayerIndex] = {
+          ["icon"] = iconLayer.icon,
+          -- itemIconSize if not icon_size specified in layer
+          ["icon_size"] = iconLayer.icon_size or itemIconSize,
+          ["scale"] = layerScale,
+          ["shift"] = layerShift,
+        }
+      end
+      return itemIcons
+
+    end
+  end
+
+  local recipeIcons = util.table.deepcopy(getItemIcons("trainassembly-recipefuel", 1, {0,0}))
+  local recipeIconsLength = #recipeIcons
+
+  for layerIndex,layerData in pairs(getItemIcons(itemPrototypeName, 0.48, {9, -9})) do
+    recipeIcons[recipeIconsLength + layerIndex] = layerData
+  end
+  return recipeIcons
+end
+
 -- We want to create different fuel recipes to create the fuel to initialy fuel the train.
 -- We make a fuel for each of the next items:
 for fuelOrder, fuelIngredient in pairs ({
@@ -11,11 +52,12 @@ for fuelOrder, fuelIngredient in pairs ({
 }) do
 
   -- For this item we create a fuel recipe
-  data:extend({
+  data:extend{
     {
       type = "recipe",
       name = "trainassembly-trainfuel-" .. fuelIngredient[1],
       localised_name = {"recipe-name.trainfuel", "trainassemblyfuel", fuelIngredient[1]},
+      icons = createRecipeIcons(fuelIngredient[1]),
       category = "advanced-crafting",
       expensive = nil,
       normal =
@@ -33,5 +75,5 @@ for fuelOrder, fuelIngredient in pairs ({
       -- recipes resulting in the same item.
       order = "a-"..fuelOrder,
     }
-  })
+  }
 end
