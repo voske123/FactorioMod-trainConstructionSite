ffcbbcbcf8ce7b04823dbba60056bfb7448442e9
LovelySanta <laurens.scheldeman@gmail.com>
2019-05-27 17:28:52 +0200

    #44 only the recipefuel item is visible now

--- data-final-fixes.lua
+++
@@ -1,4 +0,0 @@
---log(serpent.block(data.raw["gui-style"]["default"]["LSlib_default_tab_insideDeepFrame"]))
-
---local layoutTable = require("prototypes.guilayout.traindepot")
---log(serpent.block(LSlib.gui.layout.getElementPath(layoutTable, "enter-new-entry")))
