17b29df370a81e158b3fc11847ed104f6356204f
LovelySanta <laurens.scheldeman@gmail.com>
2019-08-12 16:11:29 +0200

    Added graphics for traindepot

--- prototypes/item/traindepot.lua
+++ prototypes/item/traindepot.lua
@@ -5,9 +5,15 @@ traindepot.name                  = "traindepot"
 traindepot.localised_name        = {"item-name.traindepot"}
 traindepot.localised_description = {"item-description.traindepot"}
 
-traindepot.icon = "__trainConstructionSite__/graphics/placeholders/icon.png"
-traindepot.icons = nil
-traindepot.icon_size = 32
+traindepot.icon = nil
+traindepot.icon_size = nil
+traindepot.icons = LSlib.item.getIcons(traindepot.type, "train-stop", nil, {-6, 0}, {r=40/255, g=190/255, b=220/255})
+table.insert(traindepot.icons, {
+  icon = "__trainConstructionSite__/graphics/item/traindepot/depot-128.png",
+  icon_size = 128,
+  scale = 16/128,
+  shift = {8, 8},
+})
 
 traindepot.order = "b[stop]-b[trainbuilding]"
 
