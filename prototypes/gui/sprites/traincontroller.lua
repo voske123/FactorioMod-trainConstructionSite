f7e53a0da68772ef572246db49500b804e42e258
LovelySanta <laurens.scheldeman@gmail.com>
2020-03-31 11:48:10 +0200

    updated screenshots and changelog

--- prototypes/gui/sprites/traincontroller.lua
+++ prototypes/gui/sprites/traincontroller.lua
@@ -3,7 +3,7 @@ for _,direction in pairs{"L", "R"} do
   data:extend{{
     type     = "sprite",
     name     = string.format("traincontroller-orientation-%s", direction),
-    filename = string.format("__trainConstructionSite__/graphics/placeholders/double_arrow_%s.png", direction),
+    filename = string.format("__trainConstructionSite__/graphics/sprite/double_arrow_%s.png", direction),
     width    = 64,
     height   = 64,
     scale    = .5,
